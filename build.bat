605054a715e9db6cb5171888ce60227e97cf7daf
BaDelhommer <badelhommer@gmail.com>
2025-06-07 12:06:24 -0500

    improved build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
 @echo off
 
-odin run . -debug 
+set OUT_DIR=build\debug
+
+if not exist %OUT_DIR% mkdir %OUT_DIR%
+
+odin run win32_platform.odin -file -out:%OUT_DIR%\game_debug.exe -strict-style -vet -debug
+
+echo "Debug build in %OUT_DIR%"
